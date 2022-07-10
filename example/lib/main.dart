9c1a1aa91cf4bd652f39961b881b6defc19ec1ba
wahabz <wahabz@yandex.com>
2022-07-10 09:43:55 +0100

    Add autocomplete

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:toggle_search/toggle_search.dart';
 
+List<String> kOptions = <String>[
+  'aardvark',
+  'bobcat',
+  'chameleon',
+  'dingo',
+  'elephant',
+  'flamingo',
+  'goose',
+  'hippopotamus',
+  'iguana',
+  'jaguar',
+  'koala',
+  'lemur',
+  'mouse',
+  'northern white rhinoceros',
+];
+
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
       home: Scaffold(
-    backgroundColor: Color(0xffe6aa0b),
+    backgroundColor: const Color(0xffe6aa0b),
     body: Center(
         child: SizedBox(
+      height: 42,
       width: 400,
-      child: ToggleSearch(),
+      child: ToggleSearch(options: kOptions),
     )),
   )));
 }
--- lib/toggle_search.dart
+++ lib/toggle_search.dart
@@ -1,97 +1,162 @@
 import 'package:flutter/material.dart';
 
-import 'package:animated_rotation/animated_rotation.dart' as rot;
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
-
 class ToggleSearch extends StatefulWidget {
-  const ToggleSearch(
-      {super.key,
-      this.borderRadius = 12,
-      this.iconColor = Colors.black45,
-      this.options = const [
-        "All",
-        "Videos",
-        "Images",
-        "News",
-      ]});
+  const ToggleSearch({
+    super.key,
+    required this.options,
+    this.iconColor = Colors.black45,
+    this.categories = const [
+      "All",
+      "Videos",
+      "Images",
+      "News",
+    ],
+  });
 
-  final double? borderRadius;
+  final double? borderRadius = 6;
   final Color? iconColor;
+  final List<String> options;
 
-  final List<String>? options;
-  List<String>? get items => options;
+  final List<String>? categories;
+  List<String>? get items => categories;
 
   @override
   State<ToggleSearch> createState() => _ToggleSearchState();
 }
 
 class _ToggleSearchState extends State<ToggleSearch> {
-  num _angle = 0;
+  double _angle = 0;
   int _index = 0;
+
+  final GlobalKey fieldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(widget.borderRadius!),
-      child: TextFormField(
-        autofocus: true,
-        showCursor: true,
-        style: const TextStyle(
-          color: Colors.black87,
-          height: 1.3,
-          fontWeight: FontWeight.w600,
-        ),
-        decoration: InputDecoration(
-          filled: true,
-          border: InputBorder.none,
-          fillColor: Colors.white,
-          prefixIcon: Padding(
-            padding: const EdgeInsets.only(left: 18.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    _angle = _angle + 360 / widget.options!.length;
-                    _index++;
+    return Autocomplete(
+      optionsMaxHeight: 250,
+      optionsViewBuilder: (BuildContext ccontext,
+          AutocompleteOnSelected<String> onSelected, Iterable<String> options) {
+        return Transform.translate(
+          offset: const Offset(0, -4),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Material(
+              child: Container(
+                constraints:
+                    const BoxConstraints(maxWidth: 400, maxHeight: 200),
+                key: fieldKey,
+                color: Colors.white60,
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: options.length,
+                    itemBuilder: (context, index) {
+                      final String option = options.elementAt(index);
 
-                    if (_index == widget.options!.length) {
-                      _index = 0;
-                      _angle = 0;
-                    }
-                    setState(() {});
-                  },
-                  onDoubleTap: () {
-                    _angle = 0;
-                    _index = 0;
-                    setState(() {});
-                  },
-                  child: rot.AnimatedRotation(
-                      curve: Curves.easeInOutBack,
-                      duration: const Duration(milliseconds: 400),
-                      angle: _angle,
-                      child: Icon(
-                        FontAwesomeIcons.magnifyingGlass,
-                        color: widget.iconColor,
-                        size: 20,
-                      )),
-                ),
-                const SizedBox(width: 9),
-                Text(
-                  widget.options![_index],
-                  style: GoogleFonts.montserrat(
-                    fontWeight: FontWeight.w600,
-                    color: widget.iconColor,
-                    fontSize: 18,
-                  ),
-                ),
-                const SizedBox(width: 6),
-              ],
+                      return ListTile(
+                        title: Text(option),
+                        onTap: () {
+                          onSelected(option);
+                        },
+                      );
+                    }),
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
+      fieldViewBuilder:
+          (context, textEditingController, focusNode, onFieldSubmitted) {
+        return ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(widget.borderRadius!)),
+          child: TextFormField(
+            controller: textEditingController,
+            focusNode: focusNode,
+            showCursor: true,
+            cursorRadius: const Radius.circular(6.0),
+            style: const TextStyle(
+              color: Colors.black87,
+              height: 1.4,
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+            ),
+            decoration: InputDecoration(
+              filled: true,
+              border: InputBorder.none,
+              fillColor: Colors.white,
+              prefixIcon: Padding(
+                padding: const EdgeInsets.only(left: 13.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        _angle = _angle + 360 / widget.categories!.length;
+                        _index++;
+
+                        if (_index == widget.categories!.length) {
+                          _angle = 0;
+                          _index = 0;
+                        }
+                        setState(() {});
+                      },
+                      onDoubleTap: () {
+                        _angle = 0;
+                        _index = 0;
+                        setState(() {});
+                      },
+                      child: GestureDetector(
+                        onTap: () {
+                          if (_index <= widget.categories!.length) {
+                            _angle = _angle + (1.0 / widget.categories!.length);
+                            _index++;
+                            setState(() {});
+                          }
+
+                          if (_index == widget.categories!.length) {
+                            _angle = 0;
+                            _index = 0;
+                          }
+                        },
+                        child: AnimatedRotation(
+                            duration: const Duration(milliseconds: 400),
+                            turns: _angle,
+                            curve: Curves.easeInOutBack,
+                            child: Transform.scale(
+                              scale: 1.3,
+                              child: Icon(
+                                Icons.search_rounded,
+                                color: widget.iconColor,
+                                size: 20,
+                              ),
+                            )),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Transform.scale(
+                      scale: 0.9,
+                      child: Text(
+                        widget.categories![_index],
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: widget.iconColor,
+                          height: 1.3,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      optionsBuilder: (TextEditingValue textEditingValue) {
+        return widget.options.where((String option) {
+          return option.contains(textEditingValue.text.toLowerCase());
+        });
+      },
     );
   }
 }
